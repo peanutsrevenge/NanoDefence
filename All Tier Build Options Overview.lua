76c4acaaf77730256fff7f47c31db3503ef3e382
ChrispyNut <peanutsreveng3@gmail.com>
2024-01-04 03:17:53 +0000

    Update

---
+++ All Tier Build Options Overview.lua
@@ -0,0 +1,117 @@
+Tier 1 Eco
+Early Metal - "cormakr",
+Late Metal - "coruwmakr"
+
+Early Energy - "armsolar",
+Late Energy - "armadvsol"
+Alt Energy - "corwin",
+
+Utilities = 
+
+
+
+
+
+Tier 1 Combat
+
+LR - "corpun",
+MR - "corllt",
+SR - "cormaw",
+AD - core tremor
+
+LRAA - "corflak",
+SRAA - "corrl",
+
+Support = "armrad",
+
+
+
+
+Tier 2 Eco
+Early Metal - "armmakr",
+Late Metal - "armuwmakr",
+
+Early Energy - "corsolar",
+Late Energy - "coradvsol"
+Alt Energy - "armgeo",
+
+Utilities = 
+
+
+
+
+
+Tier 2 Combat
+
+LR - "armbeamer",
+MR - "legmg",
+SR - "armclaw",
+AD - "leginf",
+
+LRAA - "corflak",
+SRAA - "armcir"
+
+Support = 
+"corrad",
+
+
+
+
+Tier 3 Eco
+Early Metal -
+Late Metal - 
+
+Early Energy - 
+Late Energy - 
+Alt Energy - 
+
+Utilities = 
+
+
+
+
+
+Tier 3 Combat
+
+LR - "corscavdtm"
+MR - "armanni",
+SR - "corhllllt",
+AD - "legministarfall",
+
+OP
+LRAA - "armflak".
+SRAA - "cormadsam",
+
+Support = 
+"armarad",
+
+
+
+Tier 4 Eco
+Early Metal -
+Late Metal - 
+
+Early Energy - 
+Late Energy - 
+Alt Energy - 
+
+Utilities = 
+
+
+
+
+
+Tier 4 Combat
+
+LR - "armannit3"
+MR - "corbuzz"
+SR - "armvulc"
+
+LRAA - 
+SRAA - 
+
+Support = 
+"corarad",
+
+
+
